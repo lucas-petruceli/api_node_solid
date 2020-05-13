7df90342d70c03043f98473b850df762a18f68e6
lucas.petruceli <lucaspmtt@gmail.com>
2020-05-13 16:38:01 -0300

    add removendo regra do hook + primeiro arquivo de teste

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -9,7 +9,7 @@ describe('SignUp Controller', () => {
 			password: 'any_password',
 			passwordConfirmation: 'any_password'
 		}
-    }
+	}
 	const httpResponse = sut.handle(httpRequest)
 	expect(httpResponse.statusCode).toBe(400)
   })
